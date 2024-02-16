e7b8ca27990a33b50afa74c618c678b32144e644
Ish Yet13 <yet13@ema.il>
2024-02-16 21:20:18 +0100

    Generating from all char classes kinda works

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use rand::seq::SliceRandom;
+
 use char_class::accu::Accumulator;
 use char_class::interval::IntervalCharClass;
 use char_class::array::ArrayCharClass;
@@ -23,32 +25,72 @@ impl GenPass {
             a_from: char,
             a_to: char,
             ) {
-        self.char_classes.push(Box::new(IntervalCharClass::new(a_from, a_to)));
+        self.char_classes
+            .push(Box::new(
+                IntervalCharClass::new(
+                        a_from, 
+                        a_to,
+                        )));
     }
 
     pub fn add_list(
             &mut self,
             a_str: &str,
             ) {
-        self.char_classes.push(Box::new(ArrayCharClass::from_exact_size_iter(a_str.chars().collect::<Vec<char>>().iter())));
+        self.char_classes
+            .push(Box::new(
+                ArrayCharClass::from_exact_size_iter(
+                    a_str
+                        .chars()
+                        .collect::<Vec<char>>()
+                        .iter()
+                        )));
+    }
+
+    fn cook_combined(&self) -> impl CharClass {
+        let mut accumulator = Accumulator::new();
+        self.char_classes
+            .iter()
+            .for_each(|char_class|
+                accumulator
+                    .collect_from(char_class)
+                    );
+        accumulator.cook_char_class()
     }
 
     pub fn generate(
             &self,
             a_num: usize,
             ) -> GenPassResult {
-        let mut accumulator = Accumulator::new();
-        self.char_classes
-            .iter()
-            .for_each(|char_class| accumulator.collect_from(char_class));
-        let combined = accumulator.cook_char_class();
-        Ok(String::from_iter(combined.take(a_num)))
+        Ok(String::from_iter(self.cook_combined().take(a_num)))
     }
 
-    // pub fn generate_all(
-    //         &self,
-    //         a_num: usize,
-    //         ) -> GenPassResult {
-    //     todo!()
-    // }
+    pub fn generate_all(
+            &mut self,
+            a_num: usize,
+            ) -> GenPassResult {
+        let num_classes = self.char_classes.len();
+        if a_num < num_classes {
+            return Err(format!(
+                    "Password too short to accomodate all {} character classes", 
+                    self.char_classes.len()
+                    ));
+        }
+        let mut buffer: Vec<char> = Vec::with_capacity(a_num);
+        self.char_classes
+            .iter_mut()
+            .map(|char_class| 
+                char_class.next().expect("Random ran out of numbers"))
+            .for_each(|c| buffer.push(c));
+        let extra_chars = a_num - num_classes;
+        if extra_chars > 0 {
+            self.cook_combined()
+                .take(extra_chars)
+                .for_each(|c| 
+                    buffer.push(c))
+        }
+        let mut rng = rand::thread_rng();
+        buffer.shuffle(&mut rng);
+        Ok(String::from_iter(buffer))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,8 @@ fn main() {
     gp.add_interval('a', 'z');
     gp.add_interval('A', 'Z');
     gp.add_list("!$%@#");
+    println!("{:?}", gp.generate(4));
     println!("{:?}", gp.generate(16));
+    println!("{:?}", gp.generate_all(4));
+    println!("{:?}", gp.generate_all(16));
 }
